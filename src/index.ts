2a7478420583c4c1bd5e6e58c31206d244772e27
avishai etach <etach89@gmail.com>
2024-09-01 13:14:55 +0300

    working to bulid

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.use(express.json());
 
 app.use(session(sessionConfig));
 
-app.use("/", ...routes);
+app.use(process.env.NODE_ENV === "production" ? "/api" : "/", ...routes);
 
 const port = process.env.PORT || 8080;
 
